525aa39b44c6f861c1abbffc4861b1bcc7f4aac9
Татьяна <106466307+TatianaVit@users.noreply.github.com>
2022-08-01 14:13:06 +0300

    feat:add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    n = Math.abs(n)
+    n = String(n)
+    n = n.split('')
+    n = n.reverse()
+    if (n.length < 3){
+      n.push('0')
+    } 
+    n = n.join('')
+    return +n
+  }
